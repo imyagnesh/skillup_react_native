076d1091ad13b818b18124faa6111dacf9d2c26a
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro.local>
2020-12-11 22:07:11 +0530

    commit

---
+++ recipe-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3359d073bdaa42b0763affd1f0291479'),
+    },
+  },
+});
---
+++ recipe-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c64a9e99-a884-42b5-8ea8-08f12233e7d0'
+};
